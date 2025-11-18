7a25ddf7f6e228e13275436f763a9a322bbc19b8
GamingDev27 <miko.gaming1975@gmail.com>
2025-11-18 17:20:42 +0800

    WIP: Layout Setup

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <div>
+  <div class="mx-8 font-medium">
    hello
   </div>
   
---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import NavBar from '@/components/NavBar/NavBar.vue';
+import Footer from '@/components/Footer/Footer.vue';
+</script>
+
+<template>
+  <header>
+    <NavBar />
+  </header>
+
+  <slot></slot>
+  
+  <footer>
+    <Footer />
+  </footer>
+</template>
+
+
+
+<style>
+
+</style>

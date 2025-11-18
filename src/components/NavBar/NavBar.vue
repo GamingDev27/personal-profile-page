05d43ce121ee14879360e898c55477d2a3c294d7
GamingDev27 <miko.gaming1975@gmail.com>
2025-11-18 21:33:15 +0800

    [Added] Navbar menu

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -2,7 +2,15 @@
 
 <template>
   <nav class="top-0 z-20 fixed bg-slate-900 pt-12 pb-4 w-full">
-    <div class="mx-auto text-slate-100 container">DXpresso</div>
+    <div class="flex justify-between mx-auto text-slate-100 container">
+      <span class="flex-1 text-[#FF6185]">DXpresso</span>
+      <div class="flex flex-1 justify-between">
+        <div>About</div>
+        <div>Experience</div>
+        <div>Projects</div>
+        <div>Contact</div>
+      </div>
+    </div>
   </nav>
 </template>
 

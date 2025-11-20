2b4e34bfb4c8fd831f5dce95e9452fa39b7efc92
GamingDev27 <miko.gaming1975@gmail.com>
2025-11-20 17:36:01 +0800

    WIP: Body background

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@ import HomeLayout from '@/layouts/HomeLayout.vue'
 </script>
 
 <template>
-  <HomeLayout> hello </HomeLayout>
+  <HomeLayout> 
+    <main class="dark pt-20">
+      <div class="bg-background/95 h-screen">
+        hello
+      </div>
+      
+    </main> 
+  </HomeLayout>
 </template>
 
 <style scoped></style>

c0df727265adb1a8d1905990fd2cda85b828a055
GamingDev27 <miko.gaming1975@gmail.com>
2025-11-23 20:39:35 +0800

    [Added] Skill.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HomeLayout from '@/layouts/HomeLayout.vue'
-import Profile from './components/profile.vue';
+import Profile from './components/Profile.vue';
+import Skills from './components/Skills.vue';
 </script>
 
 <template>
@@ -12,18 +13,7 @@ import Profile from './components/profile.vue';
       <Skills />
 
       <Skills />
-      <section class="mx-auto px-6 py-10 max-w-6xl">
-        <h2 class="mb-6 font-semibold text-3xl">Skills</h2>
-        
-        <div class="flex flex-wrap gap-3">
-          <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">JavaScript</span>
-          <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">Tailwind CSS</span>
-          <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">React</span>
-          <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">Node.js</span>
-          <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">Laravel</span>
-          <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">MySQL</span>
-        </div>
-      </section>
+      
 
       <!-- ===== Projects Section ===== -->
       <section class="mx-auto px-6 py-16 max-w-6xl">
---
+++ src/components/Skills.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <section class="mx-auto px-6 py-10 max-w-6xl">
+    <h2 class="mb-6 font-semibold text-3xl">Skills</h2>
+    
+    <div class="flex flex-wrap gap-3">
+      <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">JavaScript</span>
+      <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">Tailwind CSS</span>
+      <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">React</span>
+      <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">Node.js</span>
+      <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">Laravel</span>
+      <span class="bg-gray-100 px-4 py-2 rounded-lg text-gray-700">MySQL</span>
+    </div>
+  </section>
+</template>
+
+<style>
+
+</style>

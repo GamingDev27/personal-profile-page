ebb5fae1e379062872ea3be094cb0802acc19a42
GamingDev27 <miko.gaming1975@gmail.com>
2025-11-24 20:12:52 +0800

    [Added] project component

---
+++ src/components/Projects/Project.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+
+<template>
+    <!-- Project Card -->
+    <div class="bg-white shadow hover:shadow-lg p-5 rounded-xl transition">
+      <div class="bg-gray-200 mb-4 rounded-lg h-40"></div>
+      <h3 class="font-semibold text-xl">Project One</h3>
+      <p class="mt-2 text-gray-600">A brief description of the project goes here.</p>
+      <a href="#" class="inline-block mt-3 text-blue-600 hover:underline">View project →</a>
+    </div>
+
+</template>
+
+
+<style>
+
+</style>vue
